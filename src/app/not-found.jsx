2261155d700636cb71fc587c2a33b972c08ed4d5
shakir hussain <shakir963643@gmail.com>
2024-08-16 10:45:38 +0530

    not found and route specific route not found

---
+++ src/app/not-found.jsx
@@ -0,0 +1,9 @@
+'use client'
+export default function NotFound(){
+    return(
+        <main>
+            <h1>not found</h1>
+        </main>
+    )
+}
+// http://localhost:3000/catchAllSegment/sadfdsaf/saf/saf
