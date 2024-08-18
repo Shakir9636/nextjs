d624cc85d18b0c7154ea2745318762fad34de4c7
shakir hussain <shakir963643@gmail.com>
2024-08-18 21:26:21 +0530

    generate meta data dynamic

--- src/app/serverSideProductList/productBtn.js
+++ src/app/serverSideProductList/productBtn.js
@@ -1,6 +1,5 @@
 "use client"
 export default function ProductBtn({products}){
-    console.log(products)
     return(
         <div>
             <button onClick={()=>{console.log(products.price)}}>Click me</button>
